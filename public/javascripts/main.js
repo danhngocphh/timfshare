fadf9043c11c3770e94cdcc4ed368525584cdd0a
vegarnom <danhngocphh@gmail.com>
2021-01-20 13:51:29 +0700

    [Update] Change something

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -149,7 +149,7 @@
 
   const updateViewOnSearch = () => {
     currentPage = 'result-page';
-    $results.html('');
+  $results.html('');
   $recommend.html('');
   $recommendkey.html('');
   $pagination.css('display', 'none');
@@ -165,30 +165,24 @@
   // top: 5%;
 	// right: 3.5%;
 
-  $inputSearch.css('width', '80%');
+  $inputSearch.css('width', '79%');
   $inputSearch.css('float', 'right');
-  $inputSearch.css('margin-top', '0%');
+  $inputSearch.css('margin-top', '0.2%');
   $inputSearch.css('margin-left', '0%');
-  $domain.css('margin-top', '0%');
-  $domain.css('margin-left', '0%');
-  $domain.css('float', 'right');
-  
-  $imgSearch.css('margin-top', '1.1%');
+  $domain.css('margin-top', '0.2%');
+  $domain.css('margin-left', '12%');
+  // $domain.css('float', 'right');
+  $imgSearch.css('margin-top', '-0.8%');
   $imgSearch.css('right', '1%');
-  $imgClose.css('margin-top', '1.1%');  
+  $imgClose.css('margin-top', '-0.8%');  
   $imgClose.css('right', '2.5%');
   $imglogooutside.css('position', 'relative');
   $imglogooutside.css('width', '10%');
   $imglogooutside.css('left', '5%');
-  $imglogooutside.css('margin-top', '1%');
-  // $inputSearch.on('keyup', function(){
-  //   setTimeout(function(){
-  //     $autocomplete.css('margin-top', '20px');
-  //     $autocomplete.css('width', '20%');
-  //   },250);
-  // });
-  
+  $imglogooutside.css('margin-top', '-0.7%');
   $imglogooutside.css('float', 'left');
+  $autocomplete.css('margin-top', '20px');
+  $autocomplete.css('width', '20%');
   $recommend.css('display', 'block');
   $recommendkey.css('display', 'block');
   $hotlink.css('display', 'none');
